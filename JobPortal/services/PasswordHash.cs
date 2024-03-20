62bf1116a62b56de23164866d5dafa13a12a518e
Ashwin kumar s <ashwinskumar28406@gmail.com>
2024-03-21 00:39:23 +0530

    Sending email and hash password

---
+++ JobPortal/services/PasswordHash.cs
@@ -0,0 +1,40 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace JobPortal.services
+{
+    public  static class PasswordHash
+    {
+        public static async Task<string> ToHashSHA1(string Password)
+        {
+            return await Task.Run(() =>
+            {
+                using (SHA1Managed sha1 = new SHA1Managed())
+                {
+                    byte[] bytes = sha1.ComputeHash(Encoding.UTF8.GetBytes(Password));
+                    StringBuilder shaBuilder = new StringBuilder();
+                    foreach (byte b in bytes)
+                    {
+                        shaBuilder.Append(b.ToString("x2"));
+                    }
+                    return shaBuilder.ToString();
+                }
+            });
+        }
+    
+    public static async Task<string> GenerateSalt()
+    {
+        int saltLength = new Random().Next(8, 12);
+        byte[] salt = new byte[saltLength];
+        await Task.Run(() =>
+        {
+            using (var rng = new RNGCryptoServiceProvider())
+            {
+                rng.GetBytes(salt);
+            }
+        });
+
+        return Convert.ToBase64String(salt);
+    }
+}
+}
